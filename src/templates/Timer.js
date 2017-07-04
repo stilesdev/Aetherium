a62c5606c37b625997a964ae1e8a24280d100521
mstiles92 <mstiles92@gmail.com>
2017-07-04 03:17:11 -0400

    Link closeSession function to app instance

--- src/templates/Timer.js
+++ src/templates/Timer.js
@@ -87,6 +87,9 @@ export default {
             }
 
             this.aetherium.newScramble();
+        },
+        closeSession: function () {
+            this.aetherium.closeSession();
         }
     },
     watch: {
