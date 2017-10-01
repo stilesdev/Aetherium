b56c1dfdf17faa6d2cabb0be56015d94db3494d4
mstiles92 <mstiles92@gmail.com>
2017-10-01 00:40:54 -0400

    Remove old files no longer used

--- src/templates/History.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div id="app">
-        <div class="container-fluid">
-            <div id="historyChart"></div>
-        </div>
-    </div>
-</template>
-<script src="./History.js"></script>
-<style src="./History.css"></style>
--- src/templates/Stats.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div id="app">
-        <div class="container-fluid">
-            <div id="sessionChart"></div>
-        </div>
-    </div>
-</template>
-<script src="./Stats.js"></script>
-<style src="./Stats.css"></style>
