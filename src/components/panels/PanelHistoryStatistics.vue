ee8f533da5d5a8859dd4a7b929611f4ee5cb1c8a
mstiles92 <mstiles92@gmail.com>
2018-01-15 23:30:35 -0500

    Add best times for the current puzzle to the History view

---
+++ src/components/panels/PanelHistoryStatistics.vue
@@ -0,0 +1,75 @@
+<template>
+    <panel panelTitle="Statistics">
+        <table class="table table-condensed">
+            <tbody>
+                <tr><td>Best Single</td><td>{{ bestSingle.time }}</td><td>{{ bestSingle.date }}</td></tr>
+                <tr><td>Best Daily Mean</td><td>{{ bestDailyMean.time }}</td><td>{{ bestDailyMean.date }}</td></tr>
+                <tr><td>Best Mean of 3</td><td>{{ bestMo3.time }}</td><td>{{ bestMo3.date }}</td></tr>
+                <tr><td>Best Average of 5</td><td>{{ bestAo5.time }}</td><td>{{ bestAo5.date }}</td></tr>
+                <tr><td>Best Average of 12</td><td>{{ bestAo12.time }}</td><td>{{ bestAo12.date }}</td></tr>
+                <tr><td>Best Average of 50</td><td>{{ bestAo50.time }}</td><td>{{ bestAo50.date }}</td></tr>
+                <tr><td>Best Average of 100</td><td>{{ bestAo100.time }}</td><td>{{ bestAo100.date }}</td></tr>
+            </tbody>
+        </table>
+    </panel>
+</template>
+
+<script>
+    import * as PanelRoot from './PanelRoot.vue';
+    import { Solve } from '../../modules/Models';
+
+    function findBestSession(sessions, statistic) {
+        let session = sessions.filter(session => session[statistic] > 0).reduce((previous, current) => previous[statistic] < current[statistic] ? previous : current);
+        return { time: Solve.formatTime(session[statistic]), date: session.date };
+    }
+
+    export default {
+        data: function() {
+            return {}
+        },
+        computed: {
+            sessionsArray() {
+                let allSessions = this.$store.state.allSessions;
+                let allStats = this.$store.state.allStats;
+                let sessions = [];
+                if (allSessions && allStats) {
+                    Object.entries(allStats).forEach(entry => {
+                        let sessionId = entry[0];
+                        let stat = entry[1];
+                        stat.date = allSessions[sessionId].date;
+                        sessions.push(stat);
+                    })
+                }
+                return sessions;
+            },
+            bestSingle() {
+                return findBestSession(this.sessionsArray, 'best');
+            },
+            bestDailyMean() {
+                return findBestSession(this.sessionsArray, 'mean');
+            },
+            bestMo3() {
+                return findBestSession(this.sessionsArray, 'bestMo3')
+            },
+            bestAo5() {
+                return findBestSession(this.sessionsArray, 'bestAo5');
+            },
+            bestAo12() {
+                return findBestSession(this.sessionsArray, 'bestAo12');
+            },
+            bestAo50() {
+                return findBestSession(this.sessionsArray, 'bestAo50');
+            },
+            bestAo100() {
+                return findBestSession(this.sessionsArray, 'bestAo100');
+            }
+        },
+        components: {
+            'panel': PanelRoot
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/components/views/History.vue
+++ src/components/views/History.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="container-fluid">
         <div id="sessionHistoryChart"></div>
+        <panel-history-statistics></panel-history-statistics>
     </div>
 </template>
 
 <script src="./History.js"></script>
 <style>
     #sessionHistoryChart {
-        height: 80vh;
+        height: 60vh;
         width: 95vw;
     }
 </style>
