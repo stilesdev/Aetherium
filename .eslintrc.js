01736f4308bd601da33951ed0cf0832b74185235
Matthew Stiles <mstiles92@gmail.com>
2022-03-03 00:55:52 -0500

    chore: update eslint config, remove unneeded deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
     root: true,
     env: {
-        node: true,
+        'vue/setup-compiler-macros': true,
     },
-    // extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
-    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier'],
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-        ecmaVersion: 2020,
-    },
-    plugins: ['@typescript-eslint'],
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript/recommended', '@vue/eslint-config-prettier'],
+    ignorePatterns: ['node_modules/', 'dist/', '/*.config.[jt]s', 'public/tnoodle.js'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     },
-    overrides: [
-        {
-            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-            env: {
-                jest: true,
-            },
-        },
-    ],
 }
