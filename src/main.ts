0b44d6dd6fa8148cb4c7258933945ee196af7fa7
Matthew Stiles <mstiles92@gmail.com>
2022-03-11 00:22:11 -0500

    feat: install and initialize pinia

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { createStore } from './store'
 import { createRouter } from './router'
 import App from './components/App.vue'
 
+const pinia = createPinia()
 const store = createStore()
 const router = createRouter(store)
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(store)
 app.use(router)
 
